4edc6c9b5c12cbc5a35934d18479833158995964
shalove9895 <shalovearts0@gmail.com>
2020-08-20 10:54:30 +0530

    4th

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+(function($)
+{
+	$(function()
+	{
+		//headfix()
+		// $(window).scroll(function()
+		// {
+		// 	headfix()			
+		// })
+$('#nav-icon1').on('click',function(){
+		$(this).toggleClass('open');
+		$('body, .lines .nav').toggleClass('nav-show');
+	});
+
+$('.cur').on('click',function(){
+		$(this).toggleClass('open');
+		$('.inside').toggleClass('open');
+	});
+
+
+$('.dropdown i ').on('click',function(){
+		$(this).parent().parent().toggleClass('opened'); 
+	});
+		function headfix()
+		{
+			if($(window).scrollTop() > 100)
+			{
+				$('header').addClass('fixed')
+				$('#goToTopBtn').addClass('fixed')
+			}
+			else
+			{
+				$('header').removeClass('fixed')
+				$('#goToTopBtn').removeClass('fixed')
+			}
+		}
+	})
+
+	
+})(jQuery)
---
+++ js/custom1.js
@@ -0,0 +1,36 @@
+(function($)
+{
+	$(function()
+	{
+		headfix()
+		$(window).scroll(function()
+		{
+			headfix()			
+		})
+$('#nav-icon1').on('click',function(){
+		$(this).toggleClass('open');
+		$('body,.lines .nav').toggleClass('nav-show');
+	});
+
+$('.cur').on('click',function(){
+		$(this).toggleClass('open');
+		$('.inside').toggleClass('open');
+	});
+
+
+$('.sub').on('click',function(){
+		$(this).toggleClass('opened'); 
+	});
+		function headfix()
+		{
+			if($(window).scrollTop() > 100)
+			{
+				$('header').addClass('fixed')
+			}
+			else
+			{
+				$('header').removeClass('fixed')
+			}
+		}
+	})
+})(jQuery)
